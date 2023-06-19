e8eb9c1acf72b0d0426886625da53a1449c2f08d
Sidhant <shr.sidhant@gmail.com>
2023-06-19 18:38:44 +0530

    filter added

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 function ExpenseItem(props) {
-  const[title, setTitle] = useState(props.title);
 
-  const clickHandler = () =>{
-    setTitle('Changed');
-    console.log('ok');
 
-  }
 
 
   return (
@@ -18,10 +12,9 @@ function ExpenseItem(props) {
         <ExpenseDate date={props.date}/>
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
-      <button onClick={clickHandler}>click me</button>
     </Card>
   );
 }
