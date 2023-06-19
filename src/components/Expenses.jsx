cb79dad005ecbd6276d5f55c34645d0587b9a32d
Sidhant <shr.sidhant@gmail.com>
2023-06-19 22:37:35 +0530

    dynamic chart bars added.

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,6 +25,7 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filteredChangeHandler}
       />
+      <ExpensesChart expenses = {filteredExpenses}/>
     <ExpensesList items={filteredExpenses} />
     </Card>
   );
