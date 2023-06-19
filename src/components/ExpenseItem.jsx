11c8f964e42fd18c05069a14fb96a86f2a429799
Sidhant <shr.sidhant@gmail.com>
2023-06-19 12:41:12 +0530

    expense data successfully passes up from child component to parent

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 function ExpenseItem(props) {
+  const[title, setTitle] = useState(props.title);
+
+  const clickHandler = () =>{
+    setTitle('Changed');
+    console.log('ok');
+
+  }
 
 
   return (
@@ -10,9 +18,10 @@ function ExpenseItem(props) {
         <ExpenseDate date={props.date}/>
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
+      <button onClick={clickHandler}>click me</button>
     </Card>
   );
 }
